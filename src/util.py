3487284df7853e9d0d545275e6666af40a0ac7b7
Miguel de Mello Carpi <miguelmello@usp.br>
2023-12-20 00:55:49 -0300

    src: New Layout
    New src files structures organizes code better by separating source
    files by functionality.

--- src/util.py
+++ src/util.py
@@ -1,36 +1,44 @@
 import unicode.category as cat
 
-from languages import WALS_LANG_CODES
-from table import ExcelLoader, Key, Table
+from table import Key, Table
+from .code.wals import language_to_wals_code
+from .data.loader import ExcelLoader
+
 
 def all_but_language(lang):
-    return WALS_LANG_CODES.keys() - lang
+    return language_to_wals_code.keys() - lang
+
 
 def delete_titles_from_non_greek(table):
-    table.rm(lang=all_but_language('GREEK'), vers=0)
+    table.rm(lang=all_but_language("GREEK"), vers=0)
     return table
+
 
 def increment_greek_vers(table):
     def increment_vers(key):
         kd = key.asdict()
-        kd['vers'] += 1
+        kd["vers"] += 1
         return Key(**kd)
-    
-    table.remap(increment_vers, lang=['GREEK'])
+
+    table.remap(increment_vers, lang=["GREEK"])
     return table
+
 
 def remove_versicle_number_in_non_greek(table):
     def remove_versicle_number(vers):
-        return ' '.join(vers.split()[1:])
-    return table.map(remove_versicle_number, lang=all_but_language('GREEK'))
+        return " ".join(vers.split()[1:])
+
+    return table.map(remove_versicle_number, lang=all_but_language("GREEK"))
     return table
+
 
 def ensure_versicles_end_with_punctuation(table):
     def put_punctuation_at_end(vers):
-        vers += '' if cat(vers[-1]).startswith('P') else '.'
+        vers += "" if cat(vers[-1]).startswith("P") else "."
         return vers
 
     return table.map(put_punctuation_at_end)
+
 
 def main():
     pass
