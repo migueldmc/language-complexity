f4ee722d38d3c97f40bbdc18b0186f938b653d8f
melloguel <melloguelcrp@gmail.com>
2024-02-22 17:34:43 -0300

    src/text: tokenizer.py replaces util.py
    src/util.py - formatting

--- src/text/tokenizer.py
+++ src/text/tokenizer.py
@@ -2,16 +2,16 @@ import re
 import pandas as pd
 from collections import Counter
 
-def tokens(text, regex=r'[^ ]+'):
+
+def tokens(text, regex=r"[^ ]+"):
     return re.findall(regex, text)
+
 
 def types(tokens):
     return Counter(tokens)
 
+
 def spaces(text):
-    num_spaces = len(re.findall(r'\s+', text))
+    num_spaces = len(re.findall(r"\s+", text))
     num_non_spaces = len(text) - num_spaces
     return num_spaces, num_non_spaces
-
-
-
